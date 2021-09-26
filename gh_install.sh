a0534e9e004e38ccd5ab710447a4acaa24c176da
Antoine Carnec <antoinecarnec@gmail.com>
2021-09-26 20:07:58 +0200

    Added informative printing

--- gh_install.sh
+++ gh_install.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
-sudo pacman -Syu # Update everything
+sudo pacman -Syu || echo "Something went wrong" && exit 1 # Update everything
 
-printf "Generating github ssh key.\n"
+printf "\nGenerating github ssh key.\n"
 ssh-keygen -t rsa -f ${HOME}/.ssh/github -q -P ""
 eval "$(ssh-agent -s)"
 ssh-add ${HOME}/.ssh/github
 
-printf "Installing github cli.\n"
-sudo pacman -Syu --noconfirm github-cli
+printf "\nInstalling github cli:\n"
+sudo pacman -S --noconfirm github-cli
 gh config set git_protocol ssh
+printf "\nAuthenticating github cli:\n"
 gh auth login --web -s "delete_repo read:public_key write:public_key repo"
+printf"\nAdding ssh-key to github account\n"
 gh ssh-key add ~/.ssh/github.pub -t "${HOSTNAME}"
-mv ~/.config/nixpkgs ~/.config/nixpkgs.old
+mv ~/.config/nixpkgs ~/.config/nixpkgs.old 2> /dev/null
+printf "\nCloning dotfiles...\n"
 gh repo clone antotocar34/dotfiles ~/.config/nixpkgs
-printf "\nPlease run ./nix_install.sh.\n"
+printf "Done!\nPlease run ./nix_install.sh.\n"
